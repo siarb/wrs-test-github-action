e0b4d6f0a963e9960757543ef1624618e0b5c9d8
Siarhei <118926425+siarb@users.noreply.github.com>
2023-07-24 23:37:25 +0300

    Update action.yml

--- index.js
+++ index.js
@@ -3,15 +3,24 @@ const exec = require('@actions/exec');
 
 async function run() {
     try {
-        const imageVersion = core.getInput('image-version');
+        let imageVersion = core.getInput('image-version');
         const instance = core.getInput('instance');
+        const product = core.getInput('product');
         const artifact = core.getInput('artifact');
         const workspace = process.env.GITHUB_WORKSPACE;
 
-        const myCommands = `
+        // Set a default docker image if image-version is undefined
+        if (!imageVersion) {
+            imageVersion = '2.1.1479-p3869';
+        }
+
+        // Use product if specified, otherwise fall back to instance
+        const productOrInstance = product || instance;
+
+        const commands = `
             export DISPLAY=:99
             Xvfb :99 &
-            /opt/builder/bin/idea.sh helpbuilderinspect -source-dir /github/workspace -product ${instance} --runner github -output-dir /github/workspace/artifacts/ || true
+            /opt/builder/bin/idea.sh helpbuilderinspect -source-dir /github/workspace -product ${productOrInstance} --runner github -output-dir /github/workspace/artifacts/ || true
             echo "Test existing artifacts"
             test -e /github/workspace/artifacts/${artifact} && echo ${artifact} exists
         `;
@@ -25,7 +34,7 @@ async function run() {
             `registry.jetbrains.team/p/writerside/builder/writerside-builder:${imageVersion}`,
             '/bin/bash',
             '-c',
-            myCommands
+            commands
         ]);
     }
     catch (error) {
